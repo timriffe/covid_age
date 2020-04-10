b84aeb6afa827aec3d854d9aa30edfcd3f64c1c6
Tim Riffe <tim.riffe@gmail.com>
2020-04-11 01:23:27 +0200

    updated the readme

--- R/04_harmonize_age_groups.R
+++ R/04_harmonize_age_groups.R
@@ -23,6 +23,8 @@ inputCounts <-
 #   harmonize_age(chunk, Offsets, N = 5, OAnew = 100)
 # }
 
+
+
 outputCounts_5 <- 
   inputCounts %>% 
   group_by(Country, Code, Date, Sex, Measure) %>% 
@@ -30,7 +32,43 @@ outputCounts_5 <-
   ungroup() %>% 
   pivot_wider(names_from = Measure,
               values_from = Value) 
-head(outputCounts_5)
+
+# round output for csv
+outputCounts_5_rounded <- 
+  outputCounts_5 %>% 
+  mutate(Cases = round(Cases,1),
+         Deaths = round(Deaths,1),
+         Tests = round(Tests,1))
+
+write_csv(outputCounts_5_rounded, path = "Data/Output_5.csv")
+saveRDS(outputCounts_5, "Data/Output_5.rds")
+
+
+# Repeat for 10-year age groups
+outputCounts_10 <- 
+  inputCounts %>% 
+  group_by(Country, Code, Date, Sex, Measure) %>% 
+  do(harmonize_age(chunk = .data, Offsets, N = 10, OAnew = 100)) %>% 
+  ungroup() %>% 
+  pivot_wider(names_from = Measure,
+              values_from = Value) 
+
+# round output for csv
+outputCounts_10_rounded <- 
+  outputCounts_10 %>% 
+  mutate(Cases = round(Cases,1),
+         Deaths = round(Deaths,1),
+         Tests = round(Tests,1))
+
+write_csv(outputCounts_10_rounded, path = "Data/Output_10.csv")
+saveRDS(outputCounts_10, "Data/Output_10.rds")
+
+# Save out the inputDB
+
+write_csv(inputDB, path = "Data/inputDB.csv")
+saveRDS(inputDB, "Data/inputDB.rds")
+
+# 
 
 spot_checks <- FALSE
 if (spot_checks){
@@ -39,7 +77,7 @@ outputCounts_5 %>%
   mutate(ASCFR = Deaths / Cases,
          ASCFR = na_if(ASCFR, Deaths == 0)) %>% 
   filter(!is.na(ASCFR),
-         Sex == "m") %>% 
+         Sex == "b") %>% 
   ggplot(aes(x=Age, y = ASCFR, group = interaction(Country, Code))) + 
   geom_line(alpha=.4) + 
   scale_y_log10() + 
@@ -56,16 +94,8 @@ outputCounts_5 %>%
          ASCFR = na_if(ASCFR, Deaths == 0)) %>% 
   filter(Age == 60,
          is.na(ASCFR))
-}
 
-outputCounts_10 <- 
-  inputCounts %>% 
-  group_by(Country, Code, Date, Sex, Measure) %>% 
-  do(harmonize_age(chunk = .data, Offsets, N = 10, OAnew = 100)) %>% 
-  ungroup() %>% 
-  pivot_wider(names_from = Measure,
-              values_from = Value) 
-push_outputDB(outputCounts_5)
+
 
 #####
 Test <- outputCounts_5 %>% 
@@ -84,6 +114,8 @@ inputCounts %>%
   mutate(Diff = TOTout - TOTin) %>% 
   pull(Diff) %>% 
   summary()
+}
+
 # 
 # NYCpop2 <- c(NYCpop[1:85],yhat[86:length(yhat)])
 # 
--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # covid_age
 COVID-19 cases and deaths by age and sex
+
+What shall we name this database?
+
+This project is currently in development, stay tuned for documentation. You can get preliminary output in 5 and 10-year age groups in the `/Data` folder. This contains columns for `Cases`, `Deaths`, and `Tests` (presently only available by age for Denmark). There are more populations availale in the unharmonized input database (`inputDB.csv`) because they may be awaiting additional inputs needed to harmonize age groups.
+
+Presently, in the `inputDB` we have data for:
+Belgium, Canada, China (one snapshot only), Colombia, Denmark, France, Germany, Italy, Louisiana, Michigan, Netherlands, Portugal, South Korea, Spain, Sweden, Switzerland, USA, New York City, and Washington State.
+
+Other populations are on the way, including:
+
+- revamped Germany, including Bundesländer
+- South Africa
+- Japan
+- Brasil (possibly including regional detail)
+- Ecuador
+- Chile
+- Ontario
+- Quebec
+- Montreal
+- Michigan
+- Ohio
+- Florida
+- revamped France including deaths outside hospitals 
+- Italy regions (at least one snapshot)
+- your tip
+
+Most populations in the database contain multiple time snapshots, and all are continually monitored for new data releases. All statistics reported here are **cumulative**. 
+
+Source documentation and a methods protocol are forthcoming. These results are provisional and may change if we decide to throw out particular sources, or if we change our minds about how to split age groups, etc. So please always use the most up-to-date version.
+
+If you know of sources for other populations, please either email, Tweet, or leave an *Issue* in this repository, and we'll look into it. If you would like to assist this project in gathering data, or other tasks, please let us know and I'm sure we can find a task!
+
+## The team (so far)
+Tim Riffe, Enrique Acosta, Toni F., Catalina Torres, Jose Manuel Aburto, Cosmo Strozza, Silvia Rizzi, Emanuele del Fava, Marilia Nepomuceno, Ugofilippo Basellini, Diego Alburez, Rafael Silva, Alain Gagnon, Irwin Hecker, Christian Dudel, Alyson van Raalte, and around another ten people at the moment have made commitments. 
+
+Maybe you too?
+
+
+
+
