a347a29af0d6ac17065ebabc708000ec78817844
Tim Riffe <tim.riffe@gmail.com>
2020-06-03 13:36:49 +0200

    fix map legend

--- R/06_data_dashes.R
+++ R/06_data_dashes.R
@@ -1,6 +1,6 @@
 # availability dash
 
-rmarkdown::render(here::here('MarkdownPages/DataAvail.Rmd'), output_file = here::here('docs/DataAvail.html'))
-rmarkdown::render(here::here('MarkdownPages/PreProcessing.Rmd'), output_file = here::here('docs/DataSteps.html'))
+  rmarkdown::render(here::here('MarkdownPages/DataAvail.Rmd'), output_file = here::here('docs/DataAvail.html'))
+  rmarkdown::render(here::here('MarkdownPages/PreProcessing.Rmd'), output_file = here::here('docs/DataSteps.html'))
 
 
