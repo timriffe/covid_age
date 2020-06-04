a2955b17ccbe4c1d7e69f443f7b97af9485367d9
Tim Riffe <tim.riffe@gmail.com>
2020-06-04 12:44:05 +0200

    full build

--- R/01_grab_data.R
+++ R/01_grab_data.R
@@ -3,14 +3,16 @@ source("R/00_Functions.R")
 source("R_checks/inputDB_check.R")
 # for writing to the master input
 
-# sheets_write(dat, ssMaster, "master")
+rubric_old <- readRDS("Data/rubric_old.rds")
+# check for new data:
+rubric     <- get_input_rubric()
+saveRDS(rubric, "Data/rubric_old.rds")
 
-
-# -----------------------------------
-# THIS IS A VIOLENT RENEWAL, no checks yet. We need to make sure that the renewal
-# is lossless unless it's warranted.
-# -----------------------------------
-
+rubric_old <- rubric_old %>% select(Short, Rows)
+Updates    <- 
+  left_join(rubric, rubric_old, by = "Short") %>% 
+  mutate(Change = Rows.x - Rows.y) %>% 
+  filter(Change > 0)
 
 #check_input_updates()
 
@@ -26,14 +28,14 @@ if (check_db){
            !(Age == "UNK" & Value == 0)) 
   saveRDS(inputDB,here::here("Data/inputDBhold.rds"))
   dim(inputDB)
-  inputDB <- inputDB %>% 
-    mutate(Country = ifelse(Country == "US","USA",Country))
-  #standbyDB <- readRDS(here::here("Data/inputDB.rds"))
+  # inputDB <- inputDB %>% 
+  #   mutate(Country = ifelse(Country == "US","USA",Country))
+  # #standbyDB <- readRDS(here::here("Data/inputDB.rds"))
   
   (my_codes <- inputDB %>% pull(Short) %>% unique())
   run_checks(inputDB, my_codes)
   
-  #inputDB <- inputDB %>% mutate(Country = ifelse(Country == "US","USA",Country))
+    #inputDB <- inputDB %>% mutate(Country = ifelse(Country == "US","USA",Country))
   inputDB <-
     inputDB %>% 
     filter(Code != "CA_ON30.08.2019")
@@ -106,9 +108,13 @@ if (check_db){
   # These are special cases that we would like to account for
   # eventually, but that we don't have a protocol for at this time.
   inputDB <- inputDB %>% filter(Measure != "Probable deaths")
+  inputDB <- inputDB %>% filter(Measure != "Probable cases")
   inputDB <- inputDB %>% filter(Metric != "Rate")
   inputDB <- inputDB %>% filter(Measure != "Tested")
-  
+  inputDB <- inputDB %>% filter(Measure != "CasesProbable")
+  inputDB <- inputDB %>% filter(Measure != "CasesConfirmed")
+  inputDB <- inputDB %>% filter(Measure != "DeathsProbable")
+  inputDB <- inputDB %>% filter(Measure != "DeathsConfirmed")
   # inputDB %>% filter(Sex %in% c("F","M","unk")) %>% View()
   
   # inputDB <-
