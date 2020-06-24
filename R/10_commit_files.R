1409bee7284da6e57af11930f66b857db51e2e3e
Tim Riffe <tim.riffe@gmail.com>
2020-06-24 16:21:04 +0200

    global commit

--- R/10_commit_files.R
+++ R/10_commit_files.R
@@ -1,6 +1,12 @@
 
+
+log_section("Commit dashboards and buildlog", append = TRUE)
+library(usethis)
 library(git2r)
 repo <- init()
-commit(repo, message = "test commit. adds R/build.R")
+commit(repo, message = "global commit", all = TRUE)
+
+push()
 
 
+git_credentials(auth_token = github_token())
