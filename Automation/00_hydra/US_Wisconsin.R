5c491bf5c54712765145eba579f05b4c0835bc4d
tim riffe <tim.riffe@gmail.com>
2020-08-31 16:21:44 +0200

    update inputDB logfile

--- Automation/00_hydra/US_Wisconsin.R
+++ Automation/00_hydra/US_Wisconsin.R
@@ -8,7 +8,8 @@ setwd("C:/Users/riffe/Documents/covid_age")
 # end 
 
 # TR New: you must be in the repo environment 
-source("R/00_Functions.R")library(tidyverse)
+source("R/00_Functions.R")
+library(tidyverse)
 library(lubridate)
 library(googlesheets4)
 library(googledrive)
@@ -19,13 +20,118 @@ gs4_auth(email = email)
 rubric_i <- get_input_rubric() %>% filter(Short == "US_WI")
 ss_i     <- rubric_i %>% dplyr::pull(Sheet)
 ss_db    <- rubric_i %>% dplyr::pull(Source)
+
+cols <- structure(list(cols = list(OBJECTID = structure(list(), class = c("collector_double", 
+"collector")), GEOID = structure(list(), class = c("collector_double",
+"collector")), GEO = structure(list(), class = c("collector_character", 
+"collector")), NAME = structure(list(), class = c("collector_character", 
+"collector")), NEGATIVE = structure(list(), class = c("collector_double", 
+"collector")), POSITIVE = structure(list(), class = c("collector_double", 
+"collector")), HOSP_YES = structure(list(), class = c("collector_double", 
+"collector")), HOSP_NO = structure(list(), class = c("collector_double", 
+"collector")), HOSP_UNK = structure(list(), class = c("collector_double", 
+"collector")), POS_FEM = structure(list(), class = c("collector_double", 
+"collector")), POS_MALE = structure(list(), class = c("collector_double", 
+"collector")), POS_OTH = structure(list(), class = c("collector_double", 
+"collector")), POS_0_9 = structure(list(), class = c("collector_double", 
+"collector")), POS_10_19 = structure(list(), class = c("collector_double", 
+"collector")), POS_20_29 = structure(list(), class = c("collector_double", 
+"collector")), POS_30_39 = structure(list(), class = c("collector_double", 
+"collector")), POS_40_49 = structure(list(), class = c("collector_double", 
+"collector")), POS_50_59 = structure(list(), class = c("collector_double", 
+"collector")), POS_60_69 = structure(list(), class = c("collector_double", 
+"collector")), POS_70_79 = structure(list(), class = c("collector_double", 
+"collector")), POS_80_89 = structure(list(), class = c("collector_double", 
+"collector")), POS_90 = structure(list(), class = c("collector_double", 
+"collector")), DEATHS = structure(list(), class = c("collector_double", 
+"collector")), DTHS_FEM = structure(list(), class = c("collector_double", 
+"collector")), DTHS_MALE = structure(list(), class = c("collector_double", 
+"collector")), DTHS_OTH = structure(list(), class = c("collector_double", 
+"collector")), DTHS_0_9 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_10_19 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_20_29 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_30_39 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_40_49 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_50_59 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_60_69 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_70_79 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_80_89 = structure(list(), class = c("collector_double", 
+"collector")), DTHS_90 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_0_9 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_10_19 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_20_29 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_30_39 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_40_49 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_50_59 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_60_69 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_70_79 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_80_89 = structure(list(), class = c("collector_double", 
+"collector")), IP_Y_90 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_0_9 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_10_19 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_20_29 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_30_39 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_40_49 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_50_59 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_60_69 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_70_79 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_80_89 = structure(list(), class = c("collector_double", 
+"collector")), IP_N_90 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_0_9 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_10_19 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_20_29 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_30_39 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_40_49 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_50_59 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_60_69 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_70_79 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_80_89 = structure(list(), class = c("collector_double", 
+"collector")), IP_U_90 = structure(list(), class = c("collector_double", 
+"collector")), IC_YES = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_0_9 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_10_19 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_20_29 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_30_39 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_40_49 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_50_59 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_60_69 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_70_79 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_80_89 = structure(list(), class = c("collector_double", 
+"collector")), IC_Y_90 = structure(list(), class = c("collector_double", 
+"collector")), POS_AIAN = structure(list(), class = c("collector_double", 
+"collector")), POS_ASN = structure(list(), class = c("collector_double", 
+"collector")), POS_BLK = structure(list(), class = c("collector_double", 
+"collector")), POS_WHT = structure(list(), class = c("collector_double", 
+"collector")), POS_MLTOTH = structure(list(), class = c("collector_double", 
+"collector")), POS_UNK = structure(list(), class = c("collector_double", 
+"collector")), POS_E_HSP = structure(list(), class = c("collector_double", 
+"collector")), POS_E_NHSP = structure(list(), class = c("collector_double", 
+"collector")), POS_E_UNK = structure(list(), class = c("collector_double", 
+"collector")), DTH_AIAN = structure(list(), class = c("collector_double", 
+"collector")), DTH_ASN = structure(list(), class = c("collector_double", 
+"collector")), DTH_BLK = structure(list(), class = c("collector_double", 
+"collector")), DTH_WHT = structure(list(), class = c("collector_double", 
+"collector")), DTH_MLTOTH = structure(list(), class = c("collector_double", 
+"collector")), DTH_UNK = structure(list(), class = c("collector_double", 
+"collector")), DTH_E_HSP = structure(list(), class = c("collector_double", 
+"collector")), DTH_E_NHSP = structure(list(), class = c("collector_double", 
+"collector")), DTH_E_UNK = structure(list(), class = c("collector_double", 
+"collector")), POS_HC_Y = structure(list(), class = c("collector_double", 
+"collector")), POS_HC_N = structure(list(), class = c("collector_double", 
+"collector")), POS_HC_UNK = structure(list(), class = c("collector_double", 
+"collector")), DTH_NEW = structure(list(), class = c("collector_double", 
+"collector")), POS_NEW = structure(list(), class = c("collector_double", 
+"collector")), NEG_NEW = structure(list(), class = c("collector_double", 
+"collector")), TEST_NEW = structure(list(), class = c("collector_double", 
+"collector")), DATE = structure(list(), class = c("collector_character", 
+"collector"))), default = structure(list(), class = c("collector_guess", 
+"collector")), skip = 1), class = "col_spec")
 # reading directly from the web
 # https://data.dhsgis.wi.gov/datasets/covid-19-historical-data-table/data
-db0 <- read_csv("https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.csv",
-                col_types = cols(.default = "d"))
+db0 <- read_csv("https://opendata.arcgis.com/datasets/b913e9591eae4912b33dc5b4e88646c5_10.csv",col_types =cols)
 
 db <- db0 %>% 
-  filter(GEOID == 55)
+  filter(GEOID == "55")
 
 spec(db)
 
@@ -35,7 +141,7 @@ date_end <- as_date("2020-03-15") + as.numeric(count(db)-1)
 
 # selecting variables and reshaping to long format
 db2 <- db %>% 
-  mutate(date_f = seq(ymd(date1),ymd(date_end), by = '1 day'),
+  mutate(date_f = as_date(DATE),
          Tests = POSITIVE + NEGATIVE) %>% 
   select(date_f, POSITIVE, POS_FEM, POS_MALE, POS_0_9, POS_10_19, POS_20_29, 
          POS_30_39, POS_40_49, POS_50_59, POS_60_69, POS_70_79, 
