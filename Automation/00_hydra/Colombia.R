7a1f250893abafcfd3f974902fa415065f8ff4eb
Acosta <acosta@demogr.mpg.de>
2020-11-08 13:51:36 +0100

    colombia fix

--- Automation/00_hydra/Colombia.R
+++ Automation/00_hydra/Colombia.R
@@ -22,21 +22,17 @@ library(zip)
 drive_auth(email = email)
 gs4_auth(email = email)
 
+# cases and deaths database
 db <- read_csv("https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD",
                locale = locale(encoding = "UTF-8"))
-db_muestras <- read_csv("https://www.datos.gov.co/api/views/8835-5baf/rows.csv",
-                        locale = locale(encoding = "UTF-8"))
 
-#TODO Provintial tetst to be incorporated!!
-# changes in variable names
-# now provincial info
-# include cities
-
-db_m2 <- read_csv("https://www.datos.gov.co/api/views/8835-5baf/rows.csv?accessType=DOWNLOAD",
+# tests database
+db_m <- read_csv("https://www.datos.gov.co/api/views/8835-5baf/rows.csv?accessType=DOWNLOAD",
                         locale = locale(encoding = "UTF-8"))
 
 unique(db$Estado)
 unique(db$"Nombre municipio")
+unique(db$"Nombre departamento")
 
 
 
@@ -49,110 +45,105 @@ test <- db %>%
 db2 <- db %>% 
   rename(Sex = Sexo,
          Region = 'Nombre departamento',
-         status = 'Estado') %>% 
-  mutate(Age = as.character(Edad),
-         Region = case_when(
-           Region == "Bogotá D.C." ~ "Bogota",
-           Region == "Cartagena D.T. y C." ~ "Bolivar",
-           Region == "Norte de Santander" ~ "Nte Santander",
-           Region == "Barranquilla D.E." ~ "Atlantico",
-           Region == "Quindío" ~ "Quindio",
-           Region == "Santa Marta D.T. y C." ~ "Magdalena",
-           Region == "Archipiélago de San Andrés Providencia y Santa Catalina" ~ "San Andres",
-           Region == "Nariño" ~ "Nariño",
-           Region == "Atlántico" ~ "Atlantico",
-           Region == "Boyacá" ~ "Boyaca",
-           Region == "Córdoba" ~ "Cordoba",
-           Region == "Bolívar" ~ "Bolivar",
-           Region == "La Guajira" ~ "Guajira",
-           Region == "Buenaventura D.E." ~ "Valle del Cauca",
-           Region == "Chocó" ~ "Choco",
-           Region == "Caquetá" ~ "Caqueta",
-           Region == "Vaupés" ~ "Vaupes",
-           Region == "Guainía" ~ "Guainia",
-           TRUE ~ Region)) 
+         City = 'Nombre municipio',
+         status = 'Estado',
+         unit = 'Unidad de medida de edad') %>% 
+  mutate(Age = ifelse(unit == 1, Edad / 100, 0),
+         Region = str_to_title(Region),
+         Region = ifelse(Region == "Sta Marta D.e.", "Santa Marta", Region)) 
 
 unique(db2$Region)
+
+cities <- c("MEDELLIN",
+            "CALI")
+
+db_city <- db2 %>% 
+  filter(City %in% cities) %>% 
+  mutate(Region = str_to_title(City))
+
+db3 <- db2 %>% 
+  bind_rows(db_city)
+
+unique(db3$Age) %>% sort()
+
 # cases ----------------------------------------------
 # three dates for cases, preferred in this order: diagnosed, symptoms, reported to web
-db_cases <- db2 %>% 
-  rename(date_diag = 'Fecha diagnostico',
-         date_repo = 'fecha reporte web') %>% 
-  mutate(date_diag = ymd(str_sub(date_diag, 1, 10)),
-         date_sint = ymd(str_sub(FIS, 1, 10)),
-         date_repo = ymd(str_sub(date_repo, 1, 10)),
-         date_f = case_when(
-           !is.na(date_diag) ~ date_diag,
-           is.na(date_diag) & !is.na(date_sint) ~ date_sint,
-           is.na(date_diag) & is.na(date_sint) ~ date_repo
-         ),
+db_cases <- db3 %>% 
+  rename(date_diag = 'Fecha de diagnóstico',
+         date_repo1 = 'Fecha de notificación',
+         date_repo2 = 'fecha reporte web',
+         date_sint = 'Fecha de inicio de síntomas') %>% 
+  separate(date_diag, c("date_diag", "trash1"), sep = " ") %>% 
+  separate(date_sint, c("date_sint", "trash2"), sep = " ") %>% 
+  separate(date_repo1, c("date_repo", "trash3"), sep = " ") %>% 
+  mutate(date_diag = dmy(date_diag),
+         date_sint = dmy(date_sint),
+         date_repo = dmy(date_repo),
+         date_f = case_when(!is.na(date_diag) ~ date_diag,
+                            is.na(date_diag) & !is.na(date_sint) ~ date_sint,
+                            is.na(date_diag) & is.na(date_sint) ~ date_repo),
          Measure = "Cases") %>% 
   select(date_f, Age, Sex, Region, Measure)
 
 # deaths -----------------------------------------------------------
-db_deaths <- db2 %>% 
+db_deaths <- db3 %>% 
   filter(status == "Fallecido") %>% 
   rename(date = 'Fecha de muerte') %>% 
-  mutate(date_f = ymd(str_sub(date, 1, 10)),
+  separate(date, c("date_f", "trash1"), sep = " ") %>% 
+  mutate(date_f = dmy(date_f),
          Measure = "Deaths") %>% 
   select(date_f, Age, Sex, Region, Measure)
 
-# identifuying regions with 50+ deaths -----------------------------
-regions_deaths <- db_deaths %>% 
-  group_by(Region) %>% 
-  summarise(new = n()) %>% 
-  ungroup()
-
-reg_inc <- regions_deaths %>%
-  filter(new >= 50) %>%
-  dplyr::pull(Region)
-
 # summarising new cases for each combination -----------------------
-db3 <- bind_rows(db_cases, db_deaths) %>% 
-  mutate(Region = ifelse(Region %in% reg_inc, Region, "Resto"),
-         Sex = case_when(Sex == 'F' ~ 'f',
+db4 <- bind_rows(db_cases, db_deaths) %>% 
+  mutate(Sex = case_when(Sex == 'F' ~ 'f',
                          Sex == 'f' ~ 'f',
                          Sex == 'M' ~ 'm',
                          Sex == 'm' ~ 'm',
-                         TRUE ~ 'o')) %>% 
+                         TRUE ~ 'o'),
+         Age = as.character(Age)) %>% 
   group_by(Region, date_f, Measure, Sex, Age) %>% 
   summarise(new = n()) %>% 
   ungroup()
 
 # expanding the database to all posible combinations and cumulating values -------------
 ages <- as.character(seq(0, 100, 1))
-all_dates <- db3 %>% 
+all_dates <- db4 %>% 
   filter(!is.na(date_f)) %>% 
-  dplyr::pull(date_f) %>% 
-  unique()
+  dplyr::pull(date_f)  
 dates_f <- seq(min(all_dates), max(all_dates), by = '1 day')
 
-db4 <- db3 %>% 
+db5 <- db4 %>% 
   complete(Region, Measure, Sex, Age = ages, date_f = dates_f, fill = list(new = 0)) %>% 
+  arrange(date_f, Region, Measure, Sex, suppressWarnings(as.integer(Age))) %>% 
   group_by(Region, Measure, Sex, Age) %>% 
   mutate(Value = cumsum(new)) %>% 
   select(-new) %>% 
   ungroup()
+
+unique(db_deaths$Sex)
 
 #######################
 # template for database ------------------------------------------------------
 #######################
 
 # National data --------------------------------------------------------------
-db_co <- db4 %>% 
+db_co <- db5 %>% 
   group_by(date_f, Sex, Age, Measure) %>% 
   summarise(Value = sum(Value)) %>% 
   ungroup() %>% 
   mutate(Region = "All")
 
 # 5-year age intervals for regional data -------------------------------------
-db_regions <- db4 %>% 
-  mutate(Age2 = ifelse(as.numeric(Age) <= 4, Age, as.character(floor(as.numeric(Age)/5) * 5))) %>% 
+db_regions <- db5 %>% 
+  mutate(Age2 = as.character(floor(as.numeric(Age)/5) * 5)) %>% 
   group_by(date_f, Region, Sex, Age2, Measure) %>% 
   summarise(Value = sum(Value)) %>% 
   arrange(date_f, Region, Measure, Sex, suppressWarnings(as.integer(Age2))) %>% 
   ungroup() %>% 
   rename(Age = Age2)
+
+unique(db_regions$Region)
 
 # merging national and regional data -----------------------------------
 db_co_comp <- bind_rows(db_regions, db_co)
@@ -163,12 +154,6 @@ db_tot_age <- db_co_comp %>%
   summarise(Value = sum(Value)) %>% 
   ungroup() %>% 
   mutate(Age = "TOT")
-
-db_tot_sex <- db_co_comp %>% 
-  group_by(Region, date_f, Age, Measure) %>% 
-  summarise(Value = sum(Value)) %>% 
-  ungroup() %>% 
-  mutate(Sex = "b")
 
 db_tot <- db_co_comp %>% 
   group_by(Region, date_f, Measure) %>% 
@@ -181,35 +166,57 @@ db_inc <- db_tot %>%
   filter(Measure == "Deaths",
          Value >= 50) %>% 
   group_by(Region) %>% 
-  summarise(date_start = ymd(min(date_f)))
+  summarise(date_start = ymd(min(date_f))) %>% 
+  ungroup()
 
 # appending all data in one database ----------------------------------------------
-db_all <- bind_rows(db_co_comp, db_tot_age, db_tot_sex, db_tot)
+db_all <- bind_rows(db_co_comp, db_tot_age, db_tot)
 
 # filtering dates for each region (>50 deaths) -----------------------------------
 db_all2 <- db_all %>% 
   left_join(db_inc) %>% 
   drop_na() %>% 
-  filter((Region == "All" & date_f >= "2020-03-20") | date_f >= date_start)
+  filter((Region == "All" & date_f >= "2020-03-20") | date_f >= date_start) %>% 
+  select(-date_start)
+
+
 
 # tests data -----------------------------------
-db_m1 <- db_muestras %>% 
-  filter(Fecha != "Acumulado Feb") %>% 
-  rename(Value = Acumuladas) %>% 
-  mutate(Region = "All",
-         date_f = ymd(str_sub(Fecha, 1, 10)),
-         Sex = "b", 
+
+db_inc2 <- db_inc %>% dplyr::pull(Region)
+
+db_m_reg <- db_m %>% 
+  mutate(date_f = ymd(str_sub(Fecha, 1, 10))) %>% 
+  drop_na(date_f) %>% 
+  rename(All = Acumuladas,
+         t1 = 'Positivas acumuladas',
+         t2 = "Negativas acumuladas",
+         t3 = "Positividad acumulada",
+         t4 = "Indeterminadas",
+         t5 = "Procedencia desconocida") %>% 
+  select(-c(Fecha, t1, t2, t3, t4, t5)) %>% 
+  gather(-date_f, key = "Region", value = "Value") %>% 
+  mutate(Measure = "Tests",
          Age = "TOT",
-         Measure = "Tests") %>% 
+         Sex = "b",
+         Region = case_when(Region == "Norte de Santander" ~ "Norte Santander",
+                            Region == "Valle del Cauca" ~ "Valle",
+                            Region == "Norte de Santander" ~ "Norte Santander",
+                            TRUE ~ Region)) %>% 
+  filter(Region %in% db_inc2,
+         date_f >= "2020-03-20") %>% 
   select(Region, date_f, Sex, Age, Measure, Value) 
+
+unique(db_m_reg$Region) %>% sort()
+unique(db_all2$Region) %>% sort()
 
 # all data together in COVerAGE-DB format -----------------------------------
 db_final <- db_all2 %>%
-  bind_rows(db_m1) %>% 
+  bind_rows(db_m_reg) %>% 
   mutate(Country = "Colombia",
          AgeInt = case_when(Age == "100" ~ "5",
                             Age == "TOT" ~ "",
-                            Region == "All" | as.numeric(Age) < 5 ~ "1",
+                            Region == "All" ~ "1",
                             TRUE ~ "5"),
          Date = paste(sprintf("%02d",day(date_f)),
                       sprintf("%02d",month(date_f)),
@@ -222,10 +229,13 @@ db_final <- db_all2 %>%
            Region == "Antioquia" ~ paste0("CO_ANT", Date),
            Region == "Arauca" ~ paste0("CO_ARA", Date),
            Region == "Atlantico" ~ paste0("CO_ATL", Date),
+           Region == "Barranquilla" ~ paste0("CO_BQL", Date),
            Region == "Bolivar" ~ paste0("CO_BOL", Date),
            Region == "Boyaca" ~ paste0("CO_BOY", Date),
            Region == "Caldas" ~ paste0("CO_CAL", Date),
+           Region == "Cali" ~ paste0("CO_CLI", Date),
            Region == "Caqueta" ~ paste0("CO_CAQ", Date),
+           Region == "Cartagena" ~ paste0("CO_CAR", Date),
            Region == "Casanare" ~ paste0("CO_CAS", Date),
            Region == "Cauca" ~ paste0("CO_CAU", Date),
            Region == "Cesar" ~ paste0("CO_CES", Date),
@@ -237,17 +247,19 @@ db_final <- db_all2 %>%
            Region == "Huila" ~ paste0("CO_HUI", Date),
            Region == "Guajira" ~ paste0("CO_LAG", Date),
            Region == "Magdalena" ~ paste0("CO_MAG", Date),
+           Region == "Medellin" ~ paste0("CO_MLL", Date),
            Region == "Meta" ~ paste0("CO_MET", Date),
-           Region == "Nariño" ~ paste0("CO_NAR", Date),
-           Region == "Nte Santander" ~ paste0("CO_NSA", Date),
+           Region == "Narino" ~ paste0("CO_NAR", Date),
+           Region == "Norte Santander" ~ paste0("CO_NSA", Date),
            Region == "Putumayo" ~ paste0("CO_PUT", Date),
            Region == "Quindio" ~ paste0("CO_QUI", Date),
            Region == "Risaralda" ~ paste0("CO_RIS", Date),
            Region == "San Andres" ~ paste0("CO_SAP", Date),
+           Region == "Santa Marta" ~ paste0("CO_SMT", Date),
            Region == "Santander" ~ paste0("CO_SAN", Date),
            Region == "Sucre" ~ paste0("CO_SUC", Date),
            Region == "Tolima" ~ paste0("CO_TOL", Date),
-           Region == "Valle del Cauca" ~ paste0("CO_VAC", Date),
+           Region == "Valle" ~ paste0("CO_VAC", Date),
            Region == "Vaupes" ~ paste0("CO_VAU", Date),
            TRUE ~ paste0("CO_Other", Date)
          ),
@@ -255,97 +267,45 @@ db_final <- db_all2 %>%
   arrange(Region, date_f, Measure, Sex, suppressWarnings(as.integer(Age))) %>% 
   select(Country, Region, Code,  Date, Sex, Age, AgeInt, Metric, Measure, Value)
 
-############################################
-# slicing the database by some regions ----------------------------------
-############################################
-dims <- db_final %>% dim() 
-slice_size <- floor(dims[1]/10) 
 
-db_final_1 <- db_final[(0 * slice_size + 1) : slice_size,]
-db_final_2 <- db_final[(1 * slice_size + 1) : (2 * slice_size),]
-db_final_3 <- db_final[(2 * slice_size + 1) : (3 * slice_size),]
-db_final_4 <- db_final[(3 * slice_size + 1) : (4 * slice_size),]
-db_final_5 <- db_final[(4 * slice_size + 1) : (5 * slice_size),]
-db_final_6 <- db_final[(5 * slice_size + 1) : (6 * slice_size),]
-db_final_7 <- db_final[(6 * slice_size + 1) : (7 * slice_size),]
-db_final_8 <- db_final[(7 * slice_size + 1) : (8 * slice_size),]
-db_final_9 <- db_final[(8 * slice_size + 1) : (9 * slice_size),]
-db_final_10 <- db_final[(9 * slice_size + 1) : dims[1],]
+unique(db_final$Region)
+
+db_final_co <- db_final %>% 
+  filter(Region == "All")
+
+
 
 ############################################
 #### uploading database to Google Drive ####
 ############################################
 
+# slicing the database by some regions 
+############################################
+dims <- db_final %>% dim() 
+slice_size <- ceiling(dims[1]/10) 
+
 # TR: pull urls from rubric instead
 rubric <- get_input_rubric()
-ss_1   <- rubric %>% filter(Short == "CO_01") %>% dplyr::pull(Sheet)
-ss_2   <- rubric %>% filter(Short == "CO_02") %>% dplyr::pull(Sheet)
-ss_3   <- rubric %>% filter(Short == "CO_03") %>% dplyr::pull(Sheet)
-ss_4   <- rubric %>% filter(Short == "CO_04") %>% dplyr::pull(Sheet)
-ss_5   <- rubric %>% filter(Short == "CO_05") %>% dplyr::pull(Sheet)
-ss_6   <- rubric %>% filter(Short == "CO_06") %>% dplyr::pull(Sheet)
-ss_7   <- rubric %>% filter(Short == "CO_07") %>% dplyr::pull(Sheet)
-ss_8   <- rubric %>% filter(Short == "CO_08") %>% dplyr::pull(Sheet)
-ss_9   <- rubric %>% filter(Short == "CO_09") %>% dplyr::pull(Sheet)
-ss_10   <- rubric %>% filter(Short == "CO_10") %>% dplyr::pull(Sheet)
 
-# ss_db  <- rubric %>% filter(Short == "CO") %>% dplyr::pull(Source)
+for(i in 1:10){
+  # i <- 10
+  if (i != 10){ 
+    slice <- db_final[((i - 1) * slice_size + 1) : (i * slice_size),]
+    ss   <- rubric %>% filter(Short == paste0("CO_0", i)) %>% dplyr::pull(Sheet)
+  } else {
+    slice <- db_final[((i - 1) * slice_size + 1) : dims[1],]
+    ss   <- rubric %>% filter(Short == paste0("CO_", i)) %>% dplyr::pull(Sheet)
+  }
+  write_sheet(slice, 
+              ss = ss,
+              sheet = "database")
+  Sys.sleep(105)
+  
+}
 
-# write_sheet(db_final_co, 
-#             ss = ss_0,
-#             sheet = "database")
-# 
-write_sheet(db_final_1, 
-            ss = ss_1,
-            sheet = "database")
-
-write_sheet(db_final_2, 
-            ss = ss_2,
-            sheet = "database")
-
-Sys.sleep(105)
-
-write_sheet(db_final_3, 
-            ss = ss_3,
-            sheet = "database")
-
-write_sheet(db_final_4, 
-            ss = ss_4,
-            sheet = "database")
-
-Sys.sleep(105)
-
-write_sheet(db_final_5, 
-            ss = ss_5,
-            sheet = "database")
-
-write_sheet(db_final_6, 
-            ss = ss_6,
-            sheet = "database")
-
-Sys.sleep(105)
-
-write_sheet(db_final_7, 
-            ss = ss_7,
-            sheet = "database")
-
-write_sheet(db_final_8, 
-            ss = ss_8,
-            sheet = "database")
-
-Sys.sleep(105)
-
-write_sheet(db_final_9, 
-            ss = ss_9,
-            sheet = "database")
-
-write_sheet(db_final_10, 
-            ss = ss_10,
-            sheet = "database")
-
-Sys.sleep(105)
-
+# updating hydra automate dashboard 
 log_update(pp = "Colombia", N = dims[1])
+
 ############################################
 #### uploading metadata to Google Drive ####
 ############################################
@@ -359,7 +319,7 @@ filename_1 <- file.path("Automation/temp_files",paste0("CO", date, "cases&deaths
 filename_2 <- file.path("Automation/temp_files",paste0("CO", date, "tests.csv"))
 
 write_csv(db, filename_1)
-write_csv(db_m1, filename_2)
+write_csv(db_m, filename_2)
 
 filename <- file.path("Automation/temp_files",paste0("CO", date, "cases&deaths.zip"))
 
