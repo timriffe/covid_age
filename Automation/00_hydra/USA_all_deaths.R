48ec0653bf3192064f3000c081ad3708bb45b6c2
Jorge Cimentada <cimentadaj@gmail.com>
2020-11-30 12:01:31 +0100

    Fixes USA_all_deaths.R

--- Automation/00_hydra/USA_all_deaths.R
+++ Automation/00_hydra/USA_all_deaths.R
@@ -5,14 +5,14 @@ email <- "kikepaila@gmail.com"
 setwd("U:/gits/covid_age")
 # ##  ###
 
-# end 
+# end
 
-# TR New: you must be in the repo environment 
+# TR New: you must be in the repo environment
 source("Automation/00_Functions_automation.R")
 
 drive_auth(email = email)
 gs4_auth(email = email)
-# TR: pull urls from rubric instead 
+# TR: pull urls from rubric instead
 rubric_i <- get_input_rubric() %>% filter(Short == "US")
 ss_i     <- rubric_i %>% dplyr::pull(Sheet)
 ss_db    <- rubric_i %>% dplyr::pull(Source)
@@ -22,11 +22,11 @@ ss_db    <- rubric_i %>% dplyr::pull(Source)
 db <- read_csv("https://data.cdc.gov/api/views/vsak-wrfu/rows.csv?accessType=DOWNLOAD")
 # to <- read_csv("https://data.cdc.gov/api/views/r8kw-7aab/rows.csv?accessType=DOWNLOAD")
 
-db2 <- db %>% 
-  select("Age Group", "Sex", "End Week", "COVID-19 Deaths") %>% 
+db2 <- db %>%
+  select("Age Group", "Sex", "End Week", "COVID-19 Deaths") %>%
   rename(Age = "Age Group",
          date_f = "End Week",
-         New = "COVID-19 Deaths") %>% 
+         New = "COVID-19 Deaths") %>%
   mutate(Age = str_sub(Age, 1, 2),
          Age = case_when(Age == "Un" ~ "0",
                          Age == "Al" ~ "TOT",
@@ -42,17 +42,17 @@ db2 <- db %>%
                             Age == "5" ~ "10",
                             Age == "85" ~ "20",
                             TRUE ~ "10"),
-         date_f = make_date(d = str_sub(date_f, 4, 5), m = str_sub(date_f, 1, 2), y = 2020)) %>% 
-  select(date_f, Sex, Age, AgeInt, New) %>% 
-  arrange(date_f, Sex, Age) %>% 
+         date_f = make_date(d = str_sub(date_f, 4, 5), m = str_sub(date_f, 1, 2), y = 2020)) %>%
+  select(date_f, Sex, Age, AgeInt, New) %>%
+  arrange(date_f, Sex, Age) %>%
   drop_na()
 
-db3 <- db2 %>% 
-  group_by(Sex, Age) %>% 
+db3 <- db2 %>%
+  group_by(Sex, Age) %>%
   mutate(Value = cumsum(New))
 
-db_all <- db3 %>% 
-  filter(date_f > "2020-02-29") %>% 
+db_all <- db3 %>%
+  filter(date_f > "2020-02-29") %>%
   mutate(Country = "USA",
          Region = "All",
          Metric = "Count",
@@ -60,15 +60,15 @@ db_all <- db3 %>%
          Date = paste(sprintf("%02d", day(date_f)),
                       sprintf("%02d", month(date_f)),
                       year(date_f), sep = "."),
-         Code = paste0("US", Date)) %>% 
-  arrange(date_f, Sex, Measure, suppressWarnings(as.integer(Age))) %>% 
+         Code = paste0("US", Date)) %>%
+  arrange(date_f, Sex, Measure, suppressWarnings(as.integer(Age))) %>%
   select(Country, Region, Code, Date, Sex, Age, AgeInt, Metric, Measure, Value)
 
 ############################################
 #### uploading database to Google Drive ####
 ############################################
 
-write_sheet(db_all, 
+write_sheet(db_all,
             ss = ss_i,
             sheet = "database")
 log_update(pp = "USA_all_deaths", N = nrow(db_all))
@@ -84,12 +84,12 @@ d <- paste(sprintf("%02d", day(date_f)),
 
 sheet_name <- paste0("US_All_", d, "cases&deaths")
 
-meta <- drive_create(sheet_name, 
-                     path = ss_db, 
+meta <- drive_create(sheet_name,
+                     path = ss_db,
                      type = "spreadsheet",
                      overwrite = TRUE)
 
-write_sheet(db, 
+write_sheet(db,
             ss = meta$id,
             sheet = "deaths_age")
 
@@ -102,15 +102,13 @@ sheet_delete(meta$id, "Sheet1")
 Sys.sleep(100)
 # uploading data for INED
 # TR: not sure where this leads to, seems to be exact same place, overwriting the previous?
-meta2 <- drive_create(sheet_name, 
-                     path = "https://drive.google.com/drive/folders/1t2_JQaVJEPWEZxAqhe8TxEDkIrYeMLCF?usp=sharing", 
+meta2 <- drive_create(sheet_name,
+                     path = "https://drive.google.com/drive/folders/1t2_JQaVJEPWEZxAqhe8TxEDkIrYeMLCF?usp=sharing",
                      type = "spreadsheet",
                      overwrite = TRUE)
 
-write_sheet(db, 
+write_sheet(db,
             ss = meta2$id,
             sheet = "deaths_age")
 
 sheet_delete(meta2$id, "Sheet1")
-
-
