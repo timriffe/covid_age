6e17d6852ba4dd0afa9c47ed7f47f9dc7b334cfb
tim riffe <tim.riffe@gmail.com>
2020-12-13 12:12:50 +0100

    update inputDB logfile

--- Automation/US_OR_screencaptures.R
+++ Automation/US_OR_screencaptures.R
@@ -1,47 +1,40 @@
 
-
+#install.packages("webshot")
 library(webshot)
-
+library(lubridate)
+#install_phantomjs()
 
 
 deaths_url <- "https://public.tableau.com/profile/oregon.health.authority.covid.19#!/vizhome/OregonCOVID-19CaseDemographicsandDiseaseSeverityStatewide-SummaryTable/DemographicDataSummaryTable"
+cases_url  <- "https://public.tableau.com/profile/oregon.health.authority.covid.19#!/vizhome/OregonCOVID-19Update/CaseandTesting"
 
-cases_url <- "https://public.tableau.com/profile/oregon.health.authority.covid.19#!/vizhome/OregonCOVID-19CaseDemographicsandDiseaseSeverityStatewide/DemographicData?:display_count=y&:toolbar=n&:origin=viz_share_link&:showShareOptions=false"
 
-
-deaths_png <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/US_Oregon/US_OR_deaths",today(),".png")
-cases_png  <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Oregon/US_OR_cases",today(),".png")
+deaths_png <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/US_Oregon/US_OR_deaths_",today(),".png")
+cases_png  <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/US_Oregon/US_OR_cases_",today(),".png")
 
 webshot(url = deaths_url,
         file = deaths_png,
-        vwidth = 500,
-        vheight = 1500, 
+        vwidth = 800,
+        vheight = 3000, 
         delay = 10)
 
 
 webshot(url = cases_url,
         file = cases_png,
         vwidth = 500,
-        vheight = 1500, 
+        vheight = 2500, 
         delay = 10)
 
 
 
-library(googledrive)
-"#ember356"
 
+schedule.this <- FALSE
+if (schedule.this){
+  library(taskscheduleR)
+  taskscheduler_delete("COVerAGE-DB-US_OR_screencaptures")
+  taskscheduler_create(taskname = "COVerAGE-DB-US_OR_screencaptures", 
+                       rscript = "C:/Users/riffe/Documents/covid_age/Automation/US_OR_screencaptures.R", 
+                       schedule = "DAILY", 
+                       starttime = format(Sys.time() + 61, "%H:%M"))
+}
 
-webshot(url = "https://experience.arcgis.com/experience/85f43bd849e743cb957993a545d17170",
-        file = "VTtest.png",
-        vwidth = 500,
-        vheight=1000,
-        delay = 10,
-        selector = "#ember356")
-webshot("https://experience.arcgis.com/experience/85f43bd849e743cb957993a545d17170", "VTtest.png",
-        selector = c("#search"),
-        eval = "casper.waitForSelector('#ember356'), function () {
-    casper.click(x('#ember356'));
-    this.wait(5000);
-  });",
-        delay=3
-)
--- inputDB_compile_log.md
+++ inputDB_compile_log.md
@@ -1,26 +1,21 @@
 
-# 2020-12-12 inputDB updates 
- Sat Dec 12 20:01:08 2020 
+# 2020-12-13 inputDB updates 
+ Sun Dec 13 12:01:08 2020 
 
 
-# 2020-12-12 20:01:08 updates 
- Sat Dec 12 20:01:08 2020 
+# 2020-12-13 12:01:08 updates 
+ Sun Dec 13 12:01:08 2020 
 
 
-# Filter valid Metric entries: 
- Sat Dec 12 20:20:42 2020 
-
-Valid Metrics include: Count,Fraction,Ratio
- 14 rows removed
 # Filter valid Sex entries: 
- Sat Dec 12 20:20:43 2020 
+ Sun Dec 13 12:11:18 2020 
 
 Valid Sex values include: m,f,b,UNK
- 2529 rows removed
+ 1 rows removed
 # Duplicates detected. Following `Code`s removed: 
- Sat Dec 12 20:20:53 2020 
+ Sun Dec 13 12:11:29 2020 
 
-GB_SCO_29.03.2020
-US_IA_29.06.2020
-US_IA_29.08.2020
+PS12.10.2020
+US_CA_10.04.2020
+US_FL02.12.2020
 VE02.12.2020
