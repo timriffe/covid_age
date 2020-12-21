becc48f61317888d797e287713a05beebc41670c
Tim Riffe <tim.riffe@gmail.com>
2020-12-21 17:23:55 +0100

    add Jamaica code to trial

--- Automation/Jamaica_screenshots.R
+++ Automation/Jamaica_screenshots.R
@@ -1,3 +1,7 @@
+
+
+
+
 source("https://raw.githubusercontent.com/timriffe/covid_age/master/Automation/00_Functions_automation.R")
 
 library(lubridate)
@@ -10,7 +14,7 @@ if (!"email" %in% ls()){
 gs4_auth(email = email)
 drive_auth(email = email)
 ss_db <- get_input_rubric() %>% 
-  filter(Country == "Indonesia") %>% 
+  filter(Country == "Jamaica") %>% 
   dplyr::pull(Source)
 
 # use_python("C://Users//riffe//AppData//Local//R-MINI~1//envs//coveragedb//python.exe")
@@ -21,29 +25,29 @@ conda_install(packages = "selenium")
 py_run_string("import selenium")
 
 
-py_file <- "C:/Users/riffe/Documents/covid_age/Automation/Indonesia_screencaptures.py"
-# py_file <- "Automation/Indonesia_screencaptures.py"
+py_file <- "C:/Users/riffe/Documents/covid_age/Automation/Jamaica_screencaptures.py"
+# py_file <- "Automation/Jamaica_screencaptures.py"
 source_python(file=py_file)
 #py_run_file("C:/Users/riffe/Documents/covid_age/Automation/US_AZ_screencaptures.py")
 #py_run_file("Automation/US_AZ_screencaptures.py")
 
-file_out <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo_",today(),".png")
-file.rename("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo.png",
+file_out <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Jamaica/Jamaica_pies_",today(),".png")
+file.rename("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Jamaica/Jamaica_demo.png",
             file_out)
 
 drive_put(media = file_out,
           path = googledrive::as_id(ss_db))
 
 
-log_update("Indonesia",N = "captured")
+log_update("Jamaica",N = "captured")
 
 
 schedule.this <- FALSE
 if (schedule.this){
   library(taskscheduleR)
-  taskscheduler_delete("COVerAGE-DB-Indonesia_screencaptures")
-  taskscheduler_create(taskname = "COVerAGE-DB-Indonesia_screencaptures", 
-                       rscript = "C:/Users/riffe/Documents/covid_age/Automation/Indonesia_screencaptures.R", 
+  taskscheduler_delete("COVerAGE-DB-Jamaica_screencaptures")
+  taskscheduler_create(taskname = "COVerAGE-DB-Jamaica_screencaptures", 
+                       rscript = "C:/Users/riffe/Documents/covid_age/Automation/Jamaica_screencaptures.R", 
                        schedule = "DAILY", 
                        starttime = format(Sys.time() + 61, "%H:%M"))
 }
