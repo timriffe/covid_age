7fc4a6f451f9cc8bd3f89c6aab5e8a881a337cd6
Riffe <riffe@demogr.mpg.de>
2020-12-21 14:22:03 +0100

    further attempts for indonesia

--- Automation/Indonesia_screencaptures.py
+++ Automation/Indonesia_screencaptures.py
@@ -15,7 +15,7 @@ from time import sleep
 options=Options()
 #options.add_argument("--enable-javascript")
 #options.add_argument("--headless")
-driver = webdriver.Chrome(chrome_options=options,executable_path = path) #Path of Chrome Driver
+driver = webdriver.Chrome(options=options,executable_path = path) #Path of Chrome Driver
 #driver = webdriver.Chrome(chrome_options=options) #Path of Chrome Driver
 
 
@@ -24,10 +24,13 @@ driver.get("https://covid19.go.id/peta-sebaran")
 sleep(25)
 
 # driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[8]/div[2]/div[11]/div/div[1]').click()
-driver.find_element_by_css_selector('body > div.container-fluid > div.row > div:nth-child(8) > div.row > div:nth-child(11) > div > div.panel-heading').click()
-sleep(5)
+#driver.find_element_by_css_selector('body > div.container-fluid > div.row > div:nth-child(8) > div.row > div:nth-child(11) > div > div.panel-heading').click()
+bd = driver.find_elements_by_tag_name('body')
 
-driver.get_screenshot_as_file("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo.png")
+
+sleep(5)
+bd.screenshot("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo.png")
+#driver.get_screenshot_as_file("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo.png")
 #driver.get_screenshot_as_file("test.png")
 
 driver.quit()
