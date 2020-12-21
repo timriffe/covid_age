6d91db18b5c7196caeaceb4f7971a42e3ff1ae4e
Tim Riffe <tim.riffe@gmail.com>
2020-12-21 13:31:59 +0100

    modify indonesia to screen capture particular location of page?

--- Automation/Indonesia_screencaptures.py
+++ Automation/Indonesia_screencaptures.py
@@ -21,7 +21,10 @@ driver = webdriver.Chrome(chrome_options=options,executable_path = path) #Path o
 
 #driver = webdriver.Chrome()
 driver.get("https://covid19.go.id/peta-sebaran")
-sleep(45)
+sleep(25)
+
+driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[8]/div[2]/div[11]/div/div[2]/div/div/ul/li[1]/a"]').click()
+sleep(5)
 
 driver.get_screenshot_as_file("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo.png")
 #driver.get_screenshot_as_file("US_AZ_demo.png")
