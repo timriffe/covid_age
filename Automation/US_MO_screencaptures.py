1751b76306ccaf8477444eee7c062499ec2c6321
Tim Riffe <tim.riffe@gmail.com>
2020-12-21 22:25:39 +0100

    Missouri screen capture scripts

---
+++ Automation/US_MO_screencaptures.py
@@ -0,0 +1,44 @@
+
+"""
+Created on Thu Dec 21
+
+@author: TR
+"""
+
+
+path = "N:\COVerAGE-DB\Automation\chromedriver.exe"
+from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from time import sleep
+
+options=Options()
+#options.add_argument("--enable-javascript")
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options,executable_path = path) #Path of Chrome Driver
+#driver = webdriver.Chrome("/usr/bin/chromedriver",chrome_options=options) #Path of Chrome Driver
+
+
+
+URL = 'https://showmestrong.mo.gov/public-healthcare-demographics/'
+
+driver.get(URL)
+sleep(25)
+
+# S = lambda X: driver.execute_script('return document.body.parentNode.scroll'+X)
+# driver.set_window_size(1920,S('Height')) # May need manual adjustment
+driver.set_window_size(1920,3000)
+sleep(3)
+driver.get_screenshot_as_file("N:/COVerAGE-DB/Automation/Hydra/Data_sources/US_Missouri/US_MO_demo.png")
+#driver.get_screenshot_as_file("test.png")
+
+driver.quit()
+
+URL2 = 'https://showmestrong.mo.gov/data/public-health/
+driver = webdriver.Chrome(options=options,executable_path = path)
+driver.get(URL2)
+sleep(25)
+driver.set_window_size(1920,2000)
+sleep(3)
+driver.get_screenshot_as_file("N:/COVerAGE-DB/Automation/Hydra/Data_sources/US_Missouri/US_MO_totals.png")
+driver.quit()
