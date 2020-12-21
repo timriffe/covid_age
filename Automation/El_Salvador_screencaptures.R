ce6ae84b6271ac5d52295828bb48c2c96431fc42
Riffe <riffe@demogr.mpg.de>
2020-12-21 22:05:39 +0100

    cleanup

--- Automation/El_Salvador_screencaptures.R
+++ Automation/El_Salvador_screencaptures.R
@@ -13,8 +13,6 @@ ss_db <- get_input_rubric() %>%
   filter(Country == "El Salvador") %>% 
   dplyr::pull(Source)
 
-# use_python("C://Users//riffe//AppData//Local//R-MINI~1//envs//coveragedb//python.exe")
-#  # conda_create("coveragedb")
 #  
 use_condaenv("coveragedb", required = TRUE)
 conda_install(packages = "selenium")
@@ -22,9 +20,7 @@ py_run_string("import selenium")
 
 
 py_file <- "C:/Users/riffe/Documents/covid_age/Automation/El_Salvador_screencaptures.py"
-# py_file <- "Automation/El_Salvador_screencaptures.py"
 source_python(file=py_file)
-#py_run_file("C:/Users/riffe/Documents/covid_age/Automation/US_AZ_screencaptures.py")
 #py_run_file("Automation/US_AZ_screencaptures.py")
 
 file_out <- paste0("N:/COVerAGE-DB/Automation/Hydra/Data_sources/El_Salvador/El_Salvador_demo_",today(),".png")
@@ -36,10 +32,6 @@ drive_put(media = file_out,
 
 
 log_update("El_Salvador",N = "captured")
-# document.querySelector("#ecb6c8f2-75e1-4a0f-aae4-87ebb7d0958b > div.ContentBlock__ContentWrapper-sizwox-2.haiZJd > div > div:nth-child(38) > div > div > div > div > svg")
-# #ecb6c8f2-75e1-4a0f-aae4-87ebb7d0958b > div.ContentBlock__ContentWrapper-sizwox-2.haiZJd > div > div:nth-child(38) > div > div > div > div > svg
-# /html/body/div[2]/div/div[1]/div[1]/div[1]/div/div/div/div[1]/div/div[38]/div/div/div/div/svg
-#ecb6c8f2-75e1-4a0f-aae4-87ebb7d0958b > div.ContentBlock__ContentWrapper-sizwox-2.haiZJd > div > div:nth-child(38) > div > div > div > div > svg
 schedule.this <- FALSE
 if (schedule.this){
   library(taskscheduleR)
@@ -51,5 +43,3 @@ if (schedule.this){
 }
 
 
-
-# webshot::webshot("https://e.infogram.com/_/fx5xud0FhM7Z9NS6qpxs?src=embed&v=1?parent_url=https%3A%2F%2Fcovid19.gob.sv%2F","test.pdf",delay=45)
