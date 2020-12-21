166568d17867113896d065be4cef6a6628c8398f
Tim Riffe <tim.riffe@gmail.com>
2020-12-21 13:38:59 +0100

    modify selector indonesia

--- Automation/Indonesia_screencaptures.py
+++ Automation/Indonesia_screencaptures.py
@@ -23,11 +23,12 @@ driver = webdriver.Chrome(chrome_options=options,executable_path = path) #Path o
 driver.get("https://covid19.go.id/peta-sebaran")
 sleep(25)
 
-driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[8]/div[2]/div[11]/div/div[2]/div/div/ul/li[1]/a"]').click()
+# driver.find_element_by_xpath('/html/body/div[1]/div[1]/div[8]/div[2]/div[11]/div/div[1]').click()
+driver.find_element_by_css_selector('body > div.container-fluid > div.row > div:nth-child(8) > div.row > div:nth-child(11) > div > div.panel-heading').click()
 sleep(5)
 
 driver.get_screenshot_as_file("N:/COVerAGE-DB/Automation/Hydra/Data_sources/Indonesia/Indonesia_demo.png")
-#driver.get_screenshot_as_file("US_AZ_demo.png")
+#driver.get_screenshot_as_file("test.png")
 
 driver.quit()
 
