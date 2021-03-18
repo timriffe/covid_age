289e4308b244104fb700bab518bddd37c73a7b70
Riffe <tim.riffe@gmail.com>
2021-03-18 09:38:24 +0100

    remove use of username and password for github. switch to PAT use

--- R/10_commit_files.R
+++ R/10_commit_files.R
@@ -8,19 +8,19 @@ log_section("Commit dashboards and buildlog", append = TRUE, logfile = logfile)
 library(usethis)
 library(git2r)
 
-creds <- structure(list(username = Sys.getenv("GITHUB_USER"), 
-                        password = Sys.getenv("GITHUB_PASS")), 
-                   class = "cred_user_pass")
+# creds <- structure(list(username = Sys.getenv("GITHUB_USER"), 
+#                         password = Sys.getenv("GITHUB_PASS")), 
+#                    class = "cred_user_pass")
 
 repo <- git2r::repository(here())
 #init()
-git2r::pull(repo,credentials = creds) # possibly creds not making it this far?
+git2r::pull(repo,credentials = cred_token()) # possibly creds not making it this far?
 
 commit(repo, 
        message = "global commit", 
        all = TRUE)
 
-git2r::push(repo,credentials = creds)
+git2r::push(repo,credentials = cred_token())
 
 
 #rm(list=ls())
