064141ae0c8f5fdbee5fd6aebd3dc20e9204327d
tim riffe <tim.riffe@gmail.com>
2021-03-20 08:05:08 +0100

    auto sync with kike

--- R/a_sync.R
+++ R/a_sync.R
@@ -38,3 +38,14 @@ if (schedule_this){
     starttime = "06:00",
     startdate = format(Sys.Date() , "%d/%m/%Y"))
 }
+
+schedule_test <- FALSE
+if (schedule_test){
+  taskscheduleR::taskscheduler_delete("COVerAGE-DB-automatic-daily-sync-test")
+  taskscheduleR::taskscheduler_create(
+    taskname = "COVerAGE-DB-automatic-daily-sync-test", 
+    rscript = file.path(Sys.getenv("path_repo"),"R","a_sync.R"), 
+    schedule = "ONCE", 
+    starttime = "08:05")
+}
+
