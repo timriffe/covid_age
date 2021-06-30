7359dd0d6ef0326f6352a4b373207424c4916a57
Donzowa <jessica_d.1994@yahoo.de>
2021-06-30 10:30:48 +0200

    Fixed slovenia

--- Automation/00_hydra/00_schedule_daily_updates.R
+++ Automation/00_hydra/00_schedule_daily_updates.R
@@ -124,7 +124,7 @@ sched("US_NYC", tm = "12:48",email = auto_update_email, wd = auto_update_wd)
 sched("Austria", tm = "13:09", email = auto_update_email, wd = auto_update_wd)
 sched("Hungary", tm = "09:42", email = auto_update_email, wd = auto_update_wd)
 
-sched("Thailand", tm = "14:02", email = auto_update_email, wd = auto_update_wd)
+sched("Slovenia", tm = "10:30", email = auto_update_email, wd = auto_update_wd)
 
 
 
