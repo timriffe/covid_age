efb4cb5626b199cde3ca54c9226a6319e4101241
Enrique Acosta <kikepaila@gmail.com>
2021-10-15 09:02:17 +0200

    update inputDB logfile

--- R/01_update_inputDB.R
+++ R/01_update_inputDB.R
@@ -3,13 +3,13 @@
 # prelims
 source("https://raw.githubusercontent.com/timriffe/covid_age/master/R/00_Functions.R")
 
-change_here(wd_sched_detect())
-
-setwd(here::here())
+setwd(wd_sched_detect())
+here::i_am("covid_age.Rproj")
 startup::startup()
-# always work with the most uptodate repository
 
+# always work with the most uptodate repository
 repo <- git2r::repository(here::here())
+
 #init()
 a <- git2r::pull(repo,credentials = cred_token())
 if (class(a)[1]=="try-error"){
