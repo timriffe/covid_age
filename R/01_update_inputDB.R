e45d810c2ceb3dc91b9012ee27fdd7be5f3f769c
tim.riffe@gmail.com <tim.riffe@gmail.com>
2022-01-15 18:21:14 +0100

    rescheduling stuff

--- R/01_update_inputDB.R
+++ R/01_update_inputDB.R
@@ -263,11 +263,10 @@ if (schedule_this){
   library(taskscheduleR)
   taskscheduler_delete("COVerAGE-DB-thrice-weekly-inputDB-updates")
   taskscheduler_create(taskname = "COVerAGE-DB-thrice-weekly-inputDB-updates", 
-                       rscript =  here("R","01_update_inputDB.R"), 
+                       rscript =  here::here("R","01_update_inputDB.R"), 
                        schedule = "WEEKLY",
                        days = c("SAT","TUE","THU"),
-                       starttime = "23:07",
-                       startdate = format(Sys.Date(), "%m/%d/%Y"))
+                       starttime = "23:07")
   # 
 }
 
