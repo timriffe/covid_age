dca7b8e733d04f81baa71e3c9b0df3281286fab1
Acosta <kikepaila@gmail.com>
2022-02-08 17:18:38 +0100

    fix Chile script and adding subnational deaths

--- Automation/00_hydra/Chile.R
+++ Automation/00_hydra/Chile.R
@@ -8,12 +8,17 @@ lapply(c("tidyverse", "ggpubr", "gridExtra","readr", "googledrive", "googlesheet
 
 
 if (!"email" %in% ls()){
-  email <- "jessica_d.1994@yahoo.de"
+  # email <- "jessica_d.1994@yahoo.de"
+  email <- "kikepaila@gmail.com"
 }
 
 # info country and N drive address
 ctr <- "Chile"
 dir_n <- "N:/COVerAGE-DB/Automation/Hydra/"
+
+# Drive credentials
+drive_auth(email = email)
+gs4_auth(email = email)
 
 ################# #
 # Cases file ####
@@ -72,8 +77,8 @@ guess_chile_url <- function(days = 20){
   correct_url <- maybe_urls[dates == most_recent]
   correct_url
 }
-"https://repositoriodeis.minsal.cl/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2016_2021_18112021.zip"
-
+# "https://repositoriodeis.minsal.cl/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2016_2021_18112021.zip"
+# url_deaths <- "https://repositoriodeis.minsal.cl/DatosAbiertos/VITALES/DEFUNCIONES_FUENTE_DEIS_2016_2021_03022022.zip"
 url_deaths= guess_chile_url()
 name_deaths = str_split(url_deaths, pattern = "/") %>% unlist() %>% rev() %>% '['(1)
 name_death_file= gsub(name_deaths,pattern="zip",replacement = "csv")
@@ -92,7 +97,9 @@ name_death_file= gsub(name_deaths,pattern="zip",replacement = "csv")
 data_source_zip <- paste0(dir_n, "Data_sources/", ctr, "/death_",today(), ".zip")
 #download zip file 
 options(timeout = 120)
-download.file(url_deaths, destfile = "test.zip",  method = "curl")
+# download.file(url_deaths, destfile = "test.zip",  method = "curl")
+# download.file(url_deaths, destfile = "test.zip")
+download.file(url_deaths, destfile = data_source_zip)
 
 
 
@@ -156,31 +163,70 @@ dd1 <- dd %>%
                          TRUE ~ (EDAD_CANT - EDAD_CANT %% 5) %>% as.character()),
          Sex = case_when(GLOSA_SEXO == "Hombre" ~ "m",
                          GLOSA_SEXO == "Mujer" ~ "f",
-                         TRUE ~ "UNK")) %>% 
-  count(Date = FECHA_DEF, Sex, Age) 
+                         TRUE ~ "UNK"),
+         Region = case_when(GLOSA_REG_RES == 'De Valparaíso' ~ 'Valparaiso',
+                            GLOSA_REG_RES == 'Metropolitana de Santiago' ~ 'Metropolitana Santiago',
+                            GLOSA_REG_RES == 'De Coquimbo' ~ 'Coquimbo',
+                            GLOSA_REG_RES == 'De Atacama' ~ 'Atacama',
+                            GLOSA_REG_RES == 'Del Maule' ~ 'Maule',
+                            GLOSA_REG_RES == 'De Ñuble' ~ 'Nuble',
+                            GLOSA_REG_RES == 'De Arica y Parinacota' ~ 'Arica y Parinacota',
+                            GLOSA_REG_RES == 'Del Libertador B. OHiggins' ~ 'Libertador B. OHiggins',
+                            GLOSA_REG_RES == 'De Los Lagos' ~ 'Los Lagos',
+                            GLOSA_REG_RES == 'Del Bíobío' ~ 'Biobio',
+                            GLOSA_REG_RES == 'De Antofagasta' ~ 'Antofagasta',
+                            GLOSA_REG_RES == 'De Aisén del Gral. C. Ibáñez del Campo' ~ 'Aisen Gral. C. Ibanez Campo',
+                            GLOSA_REG_RES == 'De La Araucanía' ~ 'La Araucania',
+                            GLOSA_REG_RES == 'De Los Ríos' ~ 'Los Rios',
+                            GLOSA_REG_RES == 'De Tarapacá' ~ 'Tarapaca',
+                            GLOSA_REG_RES == 'De Magallanes y de La Antártica Chilena' ~ 'Magallanes y Antartica',
+                            TRUE ~ "UNK")) %>% 
+  count(Date = FECHA_DEF,
+        Region,
+        Sex, 
+        Age) 
+
+unique(dd$GLOSA_REG_RES)
+unique(dd1$Region)
+unique(dd1$Age)
 
 all_dates <- seq(min(dd1$Date), max(dd1$Date),by ="days")
 all_ages  <-dd1$Age %>% unique() 
 all_sexes <- c("m","f","UNK")
 
-Deaths <-
+Deaths_regions <-
   dd1 %>% 
   tidyr::complete(Date = all_dates,
-
-           Age = all_ages,
-           Sex = all_sexes,
-           fill = list(n = 0)) %>% 
-  arrange(Sex, Age, Date) %>% 
-  group_by(Sex, Age) %>% 
+                  Region,
+                  Age = all_ages,
+                  Sex = all_sexes,
+                  fill = list(n = 0)) %>% 
+  arrange(Sex, Age, Region, Date) %>% 
+  group_by(Region, Sex, Age) %>% 
   mutate(Value = cumsum(n)) %>% 
   ungroup() %>% 
   select(-n) %>% 
   mutate(Country = ctr,
-         Region = "All",
          Measure = "Deaths",
          Metric = "Count",
          Date = ddmmyyyy(Date),
-         Code = paste0("CL"),
+         Code = case_when(Region == 'Aisen Gral. C. Ibanez Campo' ~ 'CL_AI',
+                          Region == 'Antofagasta' ~ 'CL_AN',
+                          Region == 'Arica y Parinacota' ~ 'CL_AP',
+                          Region == 'Atacama' ~ 'CL_AT',
+                          Region == 'Biobio' ~ 'CL_BI',
+                          Region == 'Coquimbo' ~ 'CL_CO',
+                          Region == 'La Araucania' ~ 'CL_AR',
+                          Region == 'Libertador B. OHiggins' ~ 'CL_LI',
+                          Region == 'Los Lagos' ~ 'CL_LL',
+                          Region == 'Los Rios' ~ 'CL_LR',
+                          Region == 'Magallanes y Antartica' ~ 'CL_MA',
+                          Region == 'Maule' ~ 'CL_ML',
+                          Region == 'Metropolitana Santiago' ~ 'CL_RM',
+                          Region == 'Nuble' ~ 'CL_NB',
+                          Region == 'Tarapaca' ~ 'CL_TA',
+                          Region == 'Valparaiso' ~ 'CL_VS',
+                          TRUE ~ 'UNK'),
          AgeInt = case_when(Age == "0" ~ 1L,
                             Age == "1" ~ 4L,
                             Age == "UNK" ~ NA_integer_,
@@ -189,8 +235,20 @@ Deaths <-
   filter(! (Sex == "UNK" & Value == 0),
          ! (Age == "UNK" & Value == 0))
 
-
-out <- bind_rows(Cases, Deaths) %>% 
+Deaths_all <- 
+  Deaths_regions %>% 
+  group_by(Country, Date, Sex, Age, AgeInt, Metric, Measure) %>% 
+  summarise(Value = sum(Value)) %>% 
+  ungroup() %>% 
+  mutate(Region = "All",
+         Code = "CL")
+  
+Deaths <- 
+  bind_rows(Deaths_all,
+            Deaths_regions)
+  
+out <- 
+  bind_rows(Cases, Deaths) %>% 
   sort_input_data()
 
 dim(out)
