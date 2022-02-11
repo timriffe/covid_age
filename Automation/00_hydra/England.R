d7327dbba50de71ca72cccbbf9cc5b946b335e3e
Kniffka <maxi.s.kniffka@gmail.com>
2022-02-11 13:11:23 +0100

    Adding cases for England

--- Automation/00_hydra/England.R
+++ Automation/00_hydra/England.R
@@ -11,6 +11,7 @@ ctr          <- "England" # it's a placeholder
 dir_n        <- "N:/COVerAGE-DB/Automation/Hydra/"
 
 
+###deaths total country
 
 deaths_url <- "https://www.england.nhs.uk/statistics/wp-content/uploads/sites/2/2022/01/COVID-19-total-announced-deaths-28-January-2022.xlsx"
 
@@ -64,10 +65,74 @@ Deaths <- X %>%
   dplyr::select(Country, Region, Code, Date, 
          Sex, Age, AgeInt, Metric, Measure, Value) %>% 
   sort_input_data()
+#########################
+##cases total country####
+#########################
 
-write_rds(Deaths, paste0(dir_n, ctr, ".rds"))
+cases_url <- "https://api.coronavirus.data.gov.uk/v2/data?areaType=nation&areaCode=E92000001&metric=newCasesBySpecimenDateAgeDemographics&format=csv"
+
+data_source2 <- paste0(dir_n, "Data_sources/", ctr, "/cases",today(), ".csv")
+
+download.file(cases_url, data_source2, mode = "wb")
+
+
+# download.file(deaths_url, destfile = ("../data/ENdeaths.xlsx"))
+
+X     <- read_csv(data_source2)
+
+Cases <- X %>% 
+  select(Country =areaName, Date = date, Age = age, Value = rollingSum) %>% 
+  filter(Age != "00_59",
+         Age != "60+") %>% 
+  mutate(Region = "All",
+         Code = "UK-EN",
+         Metric = "Count",
+         Measure = "Cases",
+         Sex = "b",
+         Age = case_when(
+           Age == "00_04" ~ "0",
+           Age == "05_09" ~ "5",
+           Age == "10_14" ~ "10",
+           Age == "15_19" ~ "15",
+           Age == "20_24" ~ "20",
+           Age == "25_29" ~ "25",
+           Age == "30_34" ~ "30",
+           Age == "35_39" ~ "35",
+           Age == "40_44" ~ "40",
+           Age == "45_49" ~ "45",
+           Age == "50_54" ~ "50",
+           Age == "55_59" ~ "55",
+           Age == "60_64" ~ "60",
+           Age == "65_69" ~ "65",
+           Age == "70_74" ~ "70",
+           Age == "75_79" ~ "75",
+           Age == "80_84" ~ "80",
+           Age == "85_89" ~ "85",
+           Age == "90+" ~ "90",
+           Age == "unassigned" ~ "UNK"   
+         ),
+         AgeInt = case_when(
+           Age == "90" ~ 15L,
+           Age == "UNK" ~ NA_integer_,
+           TRUE ~ 5L
+         ),
+         Date = paste(
+           sprintf("%02d", day(Date)),
+           sprintf("%02d", month(Date)),
+           year(Date),
+           sep="."))
+  
+  
+out <- rbind(Deaths, Cases) %>% 
+  sort_input_data()
+
+
+##saving
+
+
+write_rds(out, paste0(dir_n, ctr, ".rds"))
 
 # updating hydra dashboard
-log_update(pp = ctr, N = nrow(Deaths))
+log_update(pp = ctr, N = nrow(out))
 
 
