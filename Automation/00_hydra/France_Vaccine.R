cd5b4322a086306b0fa298eff2116fdcf27527d6
Manal Kamal <59123354+manalkamal@users.noreply.github.com>
2022-07-05 16:04:01 +0200

    France_Vaccine: code is updated so that it scrap the data country-level and regional-level by age and sex

--- Automation/00_hydra/France_Vaccine.R
+++ Automation/00_hydra/France_Vaccine.R
@@ -19,7 +19,7 @@ gs4_auth(email = Sys.getenv("email"))
 ##France
 
 ###data on cases by region, sex and age
-###total france
+###total France
 ###arrounf twice as much positiv tests than cases
 #cases_tot <- read.csv2("https://www.data.gouv.fr/fr/datasets/r/dd0de5d9-b5a5-4503-930a-7b08dc0adc7c")
 #https://www.data.gouv.fr/fr/datasets/donnees-relatives-aux-resultats-des-tests-virologiques-covid-19/
@@ -28,51 +28,74 @@ gs4_auth(email = Sys.getenv("email"))
 #https://www.data.gouv.fr/fr/datasets/donnees-de-certification-electronique-des-deces-associes-au-covid-19-cepidc/
 ###data on death by sex
 
-###vaccination in france by age
-vacc <- read.csv2("https://www.data.gouv.fr/fr/datasets/r/54dd5f8d-1e2e-4ccb-8fb8-eac68245befd")
-vacc_tot_out <- vacc %>% 
-  select(Date = jour, Age = clage_vacsi, Vaccination1 = n_cum_dose1, Vaccination2 = n_cum_complet, Vaccination3 = n_cum_rappel)
-vacc_tot_out <- melt(vacc_tot_out, id = c("Date", "Age"))
-names(vacc_tot_out)[3] <- "Measure"
-names(vacc_tot_out)[4] <- "Value"
-vacc_tot_out <- vacc_tot_out %>% 
-  mutate(AgeInt = case_when(
-    Age == "0" ~ NA_integer_,
-    Age == "4" ~ 5L,
-    Age == "9" ~ 5L,
-    Age == "11" ~ 2L,
-    Age == "17" ~ 6L,
-    Age == "24" ~ 7L,
-    Age == "29" ~ 5L,
-    Age == "39" ~ 10L,
-    Age == "49" ~ 10L,
-    Age == "59" ~ 10L,
-    Age == "64" ~ 5L,
-    Age == "69" ~ 5L,
-    Age == "74" ~ 5L,
-    Age == "79" ~ 5L,
-    Age == "80" ~ 25L),
-    Age = case_when(
-      Age == "0" ~ "TOT",
-      Age == "4" ~ "0",
-      Age == "9" ~ "5",
-      Age == "11" ~ "10",
-      Age == "17" ~ "12",
-      Age == "24" ~ "18",
-      Age == "29" ~ "25",
-      Age == "39" ~ "30",
-      Age == "49" ~ "40",
-      Age == "59" ~ "50",
-      Age == "64" ~ "60",
-      Age == "69" ~ "65",
-      Age == "74" ~ "70",
-      Age == "79" ~ "75",
-      Age == "80" ~ "80"),
-    Country = "France",
-    Region = "All",
-    Metric = "Count",
-    Sex = "b"
-  ) %>% 
+###vaccination in France by age
+
+##vacc <- read.csv2("https://www.data.gouv.fr/fr/datasets/r/54dd5f8d-1e2e-4ccb-8fb8-eac68245befd")
+
+
+## MK (05.07.2022) adding:
+
+### Vaccination in France (total) by sex and age ===============
+
+##Source: https://www.data.gouv.fr/fr/datasets/donnees-relatives-aux-personnes-vaccinees-contre-la-covid-19-1/#resources
+
+vacc_total <- read.csv2("https://www.data.gouv.fr/fr/datasets/r/e6fcd558-2ff6-487b-b624-e64a8c05bb07")
+
+vacc_age_sex <- vacc_total %>% 
+  select(Date = jour, 
+         Age = clage_vacsi, 
+         Vaccination1_Homme = n_cum_dose1_h, 
+         Vaccination2_Homme = n_cum_complet_h, 
+         Vaccination3_Homme = n_cum_rappel_h,
+         Vaccination4_Homme = n_cum_2_rappel_h,
+         Vaccination1_Femme = n_cum_dose1_f, 
+         Vaccination2_Femme = n_cum_complet_f, 
+         Vaccination3_Femme = n_cum_rappel_f,
+         Vaccination4_Femme = n_cum_2_rappel_f) %>% 
+  pivot_longer(cols = -c("Date", "Age"),
+               names_to = c("Measure", "Sex"),
+               names_sep = "_",
+               values_to = "Value")
+
+
+
+vacc_tot_age_sex <- vacc_age_sex %>% 
+  mutate(Sex = case_when(Sex == "Homme" ~ "m",
+                         Sex == "Femme" ~ "f",
+                         TRUE ~ Sex),
+         AgeInt = case_when(Age == "0" ~ NA_integer_,
+                            Age == "4" ~ 5L,
+                            Age == "9" ~ 5L,
+                            Age == "11" ~ 2L,
+                            Age == "17" ~ 6L,
+                            Age == "24" ~ 7L,
+                            Age == "29" ~ 5L,
+                            Age == "39" ~ 10L,
+                            Age == "49" ~ 10L,
+                            Age == "59" ~ 10L,
+                            Age == "64" ~ 5L,
+                            Age == "69" ~ 5L,
+                            Age == "74" ~ 5L,
+                            Age == "79" ~ 5L,
+                            Age == "80" ~ 25L),
+         Age = case_when(Age == "0" ~ "TOT",
+                         Age == "4" ~ "0",
+                         Age == "9" ~ "5",
+                         Age == "11" ~ "10",
+                         Age == "17" ~ "12",
+                         Age == "24" ~ "18",
+                         Age == "29" ~ "25",
+                         Age == "39" ~ "30",
+                         Age == "49" ~ "40",
+                         Age == "59" ~ "50",
+                         Age == "64" ~ "60",
+                         Age == "69" ~ "65",
+                         Age == "74" ~ "70",
+                         Age == "79" ~ "75",
+                         Age == "80" ~ "80"),
+         Country = "France",
+         Metric = "Count",
+         Region = "All") %>% 
   arrange(Date, Age) %>% 
   mutate(
     Date = ymd(Date),
@@ -81,6 +104,116 @@ vacc_tot_out <- vacc_tot_out %>%
                  year(Date),sep="."),
     Code = paste0("FR")) %>% 
   sort_input_data()
+
+
+
+
+### Vaccination in France per region by sex and age ===============
+vacc_reg <- read.csv2("https://www.data.gouv.fr/fr/datasets/r/8e5e70fa-c082-45e3-a7b8-20862711b142")
+
+## Notes on code-book as published in the source ## 
+## we are interested in cumulative data ## so:
+## (h is for homme/ males, f for females)
+## n_cum_dose1_h : cumulative number of first dose 
+## n_cum_complet_h: cumulative number of complete vaccinations (2 doses)
+## n_cum_rappel_f: cumulative number of booster doses (3 doses)
+## n_cum_2_rappel:  Nombre cumulé de personnes vaccinées avec 2 doses de rappel
+
+
+vacc_reg_out <- vacc_reg %>% 
+  select(Region = reg,
+         Date = jour, 
+         Age = clage_vacsi, 
+         Vaccination1_Homme = n_cum_dose1_h, 
+         Vaccination2_Homme = n_cum_complet_h, 
+         Vaccination3_Homme = n_cum_rappel_h,
+         Vaccination4_Homme = n_cum_2_rappel_h,
+         Vaccination1_Femme = n_cum_dose1_f, 
+         Vaccination2_Femme = n_cum_complet_f, 
+         Vaccination3_Femme = n_cum_rappel_f,
+         Vaccination4_Femme = n_cum_2_rappel_f) %>% 
+  pivot_longer(cols = -c("Region", "Date", "Age"),
+               names_to = c("Measure", "Sex"),
+               names_sep = "_",
+               values_to = "Value")
+
+
+
+
+vacc_reg_age_sex <- vacc_reg_out %>% 
+  mutate(Sex = case_when(Sex == "Homme" ~ "m",
+                         Sex == "Femme" ~ "f",
+                         TRUE ~ Sex),
+         Code = paste0("FR-",str_pad(Region, width = 2, side = "left", pad = "0")),
+         Region = as.character(Region),
+         Region = case_when(Region == "1" ~ "Guadeloupe",
+                            Region == "2" ~ "Martinique",
+                            Region == "3" ~ "Guyane",
+                            Region == "4" ~ "La Réunion",
+                            Region == "5" ~ "Saint-Pierre-et-Miquelon",
+                            Region == "6" ~ "Mayotte",
+                            Region == "7" ~ "Saint-Barthélemy",
+                            Region == "8" ~ "Saint-Martin",
+                            Region == "11" ~ "Ile-de-France",
+                            Region == "24" ~ "Centre-Val de Loire",
+                            Region == "27" ~ "Bourgogne-Franche-Comté",
+                            Region == "28" ~ "Normandie", 
+                            Region == "32" ~ "Hauts-de-France", 
+                            Region == "44" ~ "Grand Est",
+                            Region == "52" ~ "Pays de la Loire",
+                            Region == "53" ~ "Bretagne",
+                            Region == "75" ~ "Nouvelle-Aquitaine",
+                            Region == "76" ~ "Occitanie",
+                            Region == "84" ~ "Auvergne-Rhône-Alpes",
+                            Region == "93" ~ "Provence-Alpes-Côte d’Azur",
+                            Region == "94" ~ "Corse"),
+         AgeInt = case_when(Age == "0" ~ NA_integer_,
+                       Age == "4" ~ 5L,
+                       Age == "9" ~ 5L,
+                       Age == "11" ~ 2L,
+                       Age == "17" ~ 6L,
+                       Age == "24" ~ 7L,
+                       Age == "29" ~ 5L,
+                       Age == "39" ~ 10L,
+                       Age == "49" ~ 10L,
+                       Age == "59" ~ 10L,
+                       Age == "64" ~ 5L,
+                       Age == "69" ~ 5L,
+                       Age == "74" ~ 5L,
+                       Age == "79" ~ 5L,
+                       Age == "80" ~ 25L),
+    Age = case_when(Age == "0" ~ "TOT",
+                    Age == "4" ~ "0",
+                    Age == "9" ~ "5",
+                    Age == "11" ~ "10",
+                    Age == "17" ~ "12",
+                    Age == "24" ~ "18",
+                    Age == "29" ~ "25",
+                    Age == "39" ~ "30",
+                    Age == "49" ~ "40",
+                    Age == "59" ~ "50",
+                    Age == "64" ~ "60",
+                    Age == "69" ~ "65",
+                    Age == "74" ~ "70",
+                    Age == "79" ~ "75",
+                    Age == "80" ~ "80"),
+    Country = "France",
+    Metric = "Count") %>% 
+  arrange(Date, Age) %>% 
+  mutate(
+    Date = ymd(Date),
+    Date = paste(sprintf("%02d",day(Date)),    
+                 sprintf("%02d",month(Date)),  
+                 year(Date),sep=".")) %>% 
+  sort_input_data()
+
+
+
+
+
+vacc_tot_out <- bind_rows(vacc_tot_age_sex,vacc_reg_age_sex)
+
+
 
 
 #save output 
@@ -92,7 +225,7 @@ log_update(pp = ctr, N = nrow(vacc_tot_out))
 
 #Archive 
 
-data_source <- paste0(dir_n, "Data_sources/", ctr, "/vaccine_age_",today(), ".csv")
+data_source <- paste0(dir_n, "Data_sources/", ctr, "/vaccine_age_sex_",today(), ".csv")
 
 write_csv(vacc, data_source)
 
