e80ed5c5595a03462e532f9d13345852f950e6d6
Manal Kamal <59123354+manalkamal@users.noreply.github.com>
2023-01-24 15:51:59 +0100

    update inputDB logfile

--- R/02.1_ScheduleThreeSteps.R
+++ R/02.1_ScheduleThreeSteps.R
@@ -55,8 +55,8 @@ if (schedule_this){
   taskscheduler_create(taskname = "COVerAGE-DB-thrice-weekly-inputDB-updates", 
                        rscript =  here::here("R","02.1_ScheduleThreeSteps.R"), 
                        schedule = "WEEKLY",
-                       days = c("FRI","MON"),
-                       starttime = "17:55")
+                       days = c("TUE","FRI"),
+                       starttime = "12:30")
 }
 
 
