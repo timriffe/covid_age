8bd68a0be0a289b84a40b225bae471030254c6fb
Manal Kamal <59123354+manalkamal@users.noreply.github.com>
2023-06-23 15:28:37 +0200

    update inputDB logfile

--- R/02.1_ScheduleThreeSteps.R
+++ R/02.1_ScheduleThreeSteps.R
@@ -62,7 +62,7 @@ if (schedule_this){
                        rscript =  here::here("R","02.1_ScheduleThreeSteps.R"), 
                        schedule = "WEEKLY",
                        days = c("FRI"),
-                       starttime = "13:30")
+                       starttime = "12:35")
 }
 
 
